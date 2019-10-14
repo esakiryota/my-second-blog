875009aaf2d6bbadde2caa71fd3f31c5f88c446a
esakiryota <esaki1217@gmail.com>
2019-10-14 17:13:28 +0900

    Added views to create/edit blog post inside the site.

---
+++ practiceblog/static/js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+  document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.fixed-action-btn');
+   var instances = M.FloatingActionButton.init(elems, options);
+ });
+
+ // Or with jQuery
+
+ $(document).ready(function(){
+   $('.fixed-action-btn').floatingActionButton();
+ });
+})(jQuery); // end of jQuery name space
