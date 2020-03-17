2df8a0382c1bcf9c5401a1aa27e99d95003b4044
ryota esaki <esaki1217@gmail.com>
2020-03-17 16:47:23 +0900

    email設定の追加

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,3 +131,11 @@ LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'esaki1217@gmail.com'
+EMAIL_HOST_PASSWORD = 'jvtrpsaisrqaafus'
+EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL = 'esaki1217@gmail.com'
