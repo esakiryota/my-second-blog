aa4cbdce1cba67f4d239b431db3a84864bf66ca1
ryota esaki <esaki1217@gmail.com>
2020-03-17 16:49:11 +0900

    user登録formの作成

--- practiceblog/forms.py
+++ practiceblog/forms.py
@@ -5,6 +5,13 @@ from .models import Image
 from .models import Question
 from .models import Solve
 
+from django.contrib.auth.forms import (
+    AuthenticationForm, UserCreationForm
+)
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 class PostForm(forms.ModelForm):
 
     class Meta:
@@ -31,3 +38,19 @@ class SolveForm(forms.ModelForm):
     class Meta:
         model = Solve
         fields = ('cate', 'title', 'image')
+
+class UserCreateForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'groups')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        User.objects.filter(email=email, is_active=True).delete()
+        return email
