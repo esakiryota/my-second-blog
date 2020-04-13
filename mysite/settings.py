9b632477f746800149a42c41afcf6bdd66a500e5
ryota esaki <esaki1217@gmail.com>
2020-04-14 03:28:45 +0900

    debug 変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v$i)dx-^kosepd-h@(q5!=0_skau-!x7-lw0@4dipn#r^%)*m%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = True
 
 # Application definition
 
