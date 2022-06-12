9f835ee02051cf801add912d74c06e3b24366555
esakiryota <ryota0246@gmail.com>
2022-06-12 23:06:46 +0900

    practiceblog でのソケット通信で繋げる

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,7 +5,10 @@ from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        print(self.scope)
+        if (self.scope['url_route']['kwargs'].items()):
+            self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_name = "dummy"
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
@@ -28,14 +31,16 @@ class ChatConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         type = text_data_json['type']
 
-        if type == "message":
-            message = text_data_json['message']
+        if type == "room_list":
+            room_name = text_data_json['room_name']
+            password = text_data_json['password']
             # Send message to room group
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name,
                 {
-                    'type': 'chat_message',
-                    'message': message
+                    'type': 'room_list',
+                    'room_name': room_name,
+                    'password': password
                 }
             )
         elif type == "draw":
@@ -68,13 +73,13 @@ class ChatConsumer(WebsocketConsumer):
 
 
     # Receive message from room group
-    def chat_message(self, event):
-        message = event['message']
+    # def chat_message(self, event):
+    #     message = event['message']
 
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+    #     # Send message to WebSocket
+    #     self.send(text_data=json.dumps({
+    #         'message': message
+    #     }))
 
     def draw(self, event):
         self.send(text_data=json.dumps(event))
@@ -83,5 +88,8 @@ class ChatConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps(event))
     
     def clear(self, event):
+        self.send(text_data=json.dumps(event))
+
+    def room_list(self, event):
         self.send(text_data=json.dumps(event))
         
--- mysite/asgi.py
+++ mysite/asgi.py
@@ -5,6 +5,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
+import practiceblog.routing
 
 ASGI_APPLICATION = 'mysite.asgi.application'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
@@ -14,8 +15,9 @@ application = ProtocolTypeRouter({
   "http": AsgiHandler(),
   # Just HTTP for now. (We can add other protocols later.)
   "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+        URLRouter([
+            chat.routing.websocket_urlpatterns[0],
+            practiceblog.routing.websocket_urlpatterns[0],
+        ])
     ),
 })
