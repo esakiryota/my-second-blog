29867215c066ce21da1ec0953157d9efdd88e48b
esakiryota <ryota0246@gmail.com>
2022-06-16 18:28:03 +0900

    whiteboard 調整

--- practiceblog/api/apis.py
+++ practiceblog/api/apis.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponse
 from django.http import JsonResponse
-from django.http import QueryDict
 import json
 
 
@@ -10,12 +8,12 @@ def updateRoom(request, room_name):
     }
     data = request.body
     data = json.loads(data);
-    with open(f'chat/board/{room_name}.json', "w") as f:
+    with open(f'practiceblog/boards/{room_name}.json', "w") as f:
         json.dump(data, f)
     return JsonResponse(d)
 
 def loadRoom(request, room_name):
-    json_open = open(f'chat/board/{room_name}.json', 'r')
+    json_open = open(f'practiceblog/boards/{room_name}.json', 'r')
     json_load = json.load(json_open)
     print(json_load)
     return JsonResponse({"data": json_load})
